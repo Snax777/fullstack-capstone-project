663cfe4faa894e147a9bcbda2bcef2c277606d63
Snax777 <sinethembatompelo3@gmail.com>
2025-08-26 00:02:50 -0400

    More Front End Changes

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -5,6 +5,7 @@ import DetailsPage from './components/DetailsPage/DetailsPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import SearchPage from './components/SearchPage/SearchPage';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
+          <Route path="/app/search" element={<SearchPage/>} />
         </Routes>
         </>
   );
